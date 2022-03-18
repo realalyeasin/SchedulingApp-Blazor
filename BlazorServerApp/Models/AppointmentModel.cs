db9902fd8902590f0c4ef3ac3b2f171b0e7998f7
Yeasin <realalyeasin@gmail.com>
2022-03-18 17:11:34 +0600

    crud

--- BlazorServerApp/Models/AppointmentModel.cs
+++ BlazorServerApp/Models/AppointmentModel.cs
@@ -1,21 +1,21 @@
 ﻿namespace BlazorServerApp.Models
 {
-    public class AppointmentModel
-    {
-        public int Id { get; set; }
+	public class AppointmentModel
+	{
+		public int Id { get; set; }
 
-        public string AvailableAppointmentId { get; set; }
+		public string AvailableAppointmentId { get; set; }
 
-        public string FullName { get; set; } = string.Empty;
+		public string FullName { get; set; } = string.Empty;
 
-        public string Reason { get; set; } = string.Empty;
+		public string Reason { get; set; } = string.Empty;
 
-        public DateTime StartTime { get; set; }
+		public DateTime StartTime { get; set; }
 
-        public DateTime EndTime { get; set; }
+		public DateTime EndTime { get; set; }
 
-        public DateTime CreatedAt { get; set; }
+		public DateTime CreatedAt { get; set; }
 
-        public DateTime UpdatedAt { get; set; }
-    }
+		public DateTime UpdatedAt { get; set; }
+	}
 }
